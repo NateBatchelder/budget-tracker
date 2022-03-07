0b4069c2ed83c0152fe15359d660b8c802738a94
Nathan Batchelder <natebatchelder@gmail.com>
2022-03-06 20:07:09 -0800

    working on idb branch/ file

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,8 +1,61 @@
 // db variable
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradedneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+request.onsuccess = function (event) {
+  db = event.target.result;
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+request.onerror = function (event) {
+  console.log("[ServiceWorker] Error");
+};
 
 // save record
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+function searchDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
 
-// upload record (user entry)
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse.message);
+          }
+          // upload record (user entry)
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
 
+          alert("record sent to server");
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+}
 
-// event listner for form submit when online 
+// event listner for form submit when online
+window.addEventListener("online", checkDatabase);
